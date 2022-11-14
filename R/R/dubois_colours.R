5c0ec9e6e265fd2d7b3687fe8d7f9d85a54cdcbb
marcboschmatas <64325179+marcboschmatas@users.noreply.github.com>
2022-11-14 17:51:32 +0100

    Added vignettes, corrections

---
+++ R/R/dubois_colours.R
@@ -0,0 +1,21 @@
+## This are colours based on WEB DuBois's plots and two palettes.
+#' @export
+dubois_yellow <- "#ebc246"
+#' @export
+dubois_green <- "#00843E"
+#' @export
+dubois_red <- "#dc143c"
+#' @export
+dubois_blue <- "#2e28d4"
+#' @export
+dubois_brown <- "#2B1C00"
+#' @export
+dubois_black <- "#242424"
+#' @export
+dubois_pink <- "#ffc0cb"
+#' @export
+dubois_tan <- "#d2b48c"
+#' @export
+dubois_palette_long <- c(dubois_green, dubois_yellow, dubois_pink,dubois_red, dubois_tan, dubois_brown, dubois_blue, dubois_black)
+#' @export
+dubois_palette_short <- c(dubois_green, dubois_yellow, dubois_red, dubois_brown, dubois_black)
