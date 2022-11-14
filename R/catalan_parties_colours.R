897b80010ee415d84915e20fff3190ca9d8af11e
marcboschmatas <64325179+marcboschmatas@users.noreply.github.com>
2022-11-14 16:57:57 +0100

    1st commit

---
+++ R/catalan_parties_colours.R
@@ -0,0 +1,67 @@
+#' @title catparties_palette
+#' @description Official colours for Catalan political parties 2010-2022
+#' Colors oficials dels partits polítics catalans 2010-2022
+#' JxCat es refereix a la Coalició JxCat que es va presentar a les eleccions del 2017 (rosat). Junts, al partit polític posterior (turquesa)
+#' JxCat refers to the JxCat coalition that ran on the 2017 election (pink). Junts, to its successor party (turquoise)
+#' #' Names are usually given in abbreviated form unless not frequently used in daily speech. It should be trivial to change it.
+#' @export
+catparties_palette <- c("PSC" = "#E73B39", "ERC" = "#FFB232", "JxS" = "#3AB6A5",
+                        "CUP" = "#ffed00", "PP" = "#0bb2ff", "C's" = "#EB6109", 
+                        "ICV.EUIA" = "#009966", "CiU" = "#18307B", "JxCat" = "#ED5975",
+                        "Junts" = "#00C3B2", "Solidaritat" = "#DB0828", "Democràcia i Llibertat" = "#212765", 
+                        "Demòcrates" = "#1475cf", "En Comú Podem" = "#6E236E", "Barcelona en Comú" = "#E65C56",
+                        "Vox" = "#63BE21")
+
+#' @export
+color_psc <- "#E73B39"
+
+
+#' @export
+color_erc <- "#FFB232"
+
+
+#' @export
+color_jxsi <- "#3AB6A5"
+
+#' @export
+color_cup <- "#ffed00"
+
+#' @export
+
+color_pp <- "#0bb2ff"
+
+#' @export
+color_cs <- "#EB6109"
+
+#' @export
+color_icv <- "#009966"
+
+#' @export
+color_ciu <- "#18307B"
+
+#' @export
+color_jxcat <- "#ED5975"
+
+#' @export
+color_junts <- "#00C3B2"
+
+#' @export
+color_solidaritat <- "#DB0828"
+
+#' @export
+color_democracia_llibertat <- "#212765"
+
+#' @export
+color_democrates <- "#1475cf"
+
+#' @export
+color_ecp <- "#6E236E"
+
+#' @export
+color_bec <- "#E65C56"
+
+#' @export
+color_vox <- "#63BE21"
+
+
+
